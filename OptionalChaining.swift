1fa731061bf69f067e1ea0b3b1e3fdeb201308f3
Zhou Yang <zy856n@att.com>
2014-07-24 18:31:32 -0400

    Extension for Array, Bool, String and more

--- OptionalChaining.swift
+++ OptionalChaining.swift
@@ -10,3 +10,114 @@ import Foundation
 extension NSObject {
     func then(next: NSObject?) -> NSObject? { return next }
 }
+extension Array {
+    func then(next: Any?) -> Any? { return next }
+}
+extension Bool {
+    func then(next: Any?) -> Any? { return next }
+}
+extension Dictionary {
+    func then(next: Any?) -> Any? { return next }
+}
+extension Double {
+    func then(next: Any?) -> Any? { return next }
+}
+extension Float {
+    func then(next: Any?) -> Any? { return next }
+}
+extension Float96 {
+    func then(next: Any?) -> Any? { return next }
+}
+extension Int {
+    func then(next: Any?) -> Any? { return next }
+}
+extension Int16 {
+    func then(next: Any?) -> Any? { return next }
+}
+extension Int32 {
+    func then(next: Any?) -> Any? { return next }
+}
+extension Int64 {
+    func then(next: Any?) -> Any? { return next }
+}
+extension Int8 {
+    func then(next: Any?) -> Any? { return next }
+}
+extension String {
+    func then(next: Any?) -> Any? { return next }
+}
+extension UInt {
+    func then(next: Any?) -> Any? { return next }
+}
+extension UInt16 {
+    func then(next: Any?) -> Any? { return next }
+}
+extension UInt32 {
+    func then(next: Any?) -> Any? { return next }
+}
+extension UInt64 {
+    func then(next: Any?) -> Any? { return next }
+}
+extension UInt8 {
+    func then(next: Any?) -> Any? { return next }
+}
+extension UTF8 {
+    func then(next: Any?) -> Any? { return next }
+}
+extension UTF16 {
+    func then(next: Any?) -> Any? { return next }
+}
+extension UTF32 {
+    func then(next: Any?) -> Any? { return next }
+}
+extension FloatingPointClassification {
+    func then(next: Any?) -> Any? { return next }
+}
+extension ContiguousArray {
+    func then(next: Any?) -> Any? { return next }
+}
+extension COpaquePointer {
+    func then(next: Any?) -> Any? { return next }
+}
+extension DictionaryGenerator {
+    func then(next: Any?) -> Any? { return next }
+}
+extension DictionaryIndex {
+    func then(next: Any?) -> Any? { return next }
+}
+extension EmptyCollection {
+    func then(next: Any?) -> Any? { return next }
+}
+extension EmptyGenerator {
+    func then(next: Any?) -> Any? { return next }
+}
+extension FilterCollectionView {
+    func then(next: Any?) -> Any? { return next }
+}
+extension FilterCollectionViewIndex {
+    func then(next: Any?) -> Any? { return next }
+}
+extension FilterGenerator {
+    func then(next: Any?) -> Any? { return next }
+}
+extension HeapBuffer {
+    func then(next: Any?) -> Any? { return next }
+}
+extension HeapBufferStorageBase {
+    func then(next: Any?) -> Any? { return next }
+}
+extension ImplicitlyUnwrappedOptional {
+    func then(next: Any?) -> Any? { return next }
+}
+extension MapCollectionView {
+    func then(next: Any?) -> Any? { return next }
+}
+extension MapSequenceGenerator {
+    func then(next: Any?) -> Any? { return next }
+}
+extension Range {
+    func then(next: Any?) -> Any? { return next }
+}
+extension UnsafePointer {
+    func then(next: Any?) -> Any? { return next }
+}
