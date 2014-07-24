615372bc75b0c318217eba11b1d42af3dc7d2209
Zhou Yang <zy856n@att.com>
2014-07-24 16:10:26 -0400

    Initial commit

---
+++ OptionalChaining.swift
@@ -0,0 +1,12 @@
+//
+//  OptionalChaining.swift
+//
+//  Created by Yang Zhou on 7/24/14.
+//  Copyright (c) 2014 Yang Zhou. All rights reserved.
+//
+
+import Foundation
+
+extension NSObject {
+    func then(next: NSObject?) -> NSObject? { return next }
+}
